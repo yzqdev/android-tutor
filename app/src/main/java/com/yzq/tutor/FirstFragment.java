1ecb78ddd7df27d8f38f74bd5b76d2ef1bd7bcb1
yzqdev <yzqdev@outlook.com>
2021-08-03 15:30:11 +0800

    添加网络权限和网络请求

--- app/src/main/java/com/yzq/tutor/FirstFragment.java
+++ app/src/main/java/com/yzq/tutor/FirstFragment.java
@@ -3,15 +3,25 @@ package com.yzq.tutor;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
 import com.yzq.tutor.databinding.FragmentFirstBinding;
+
+import java.io.IOException;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class FirstFragment extends Fragment {
 
@@ -30,14 +40,47 @@ public class FirstFragment extends Fragment {
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        if (android.os.Build.VERSION.SDK_INT > 9) {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+        }
         binding.actBtn.setOnClickListener(v -> {
             binding.actBtn.setText("刻晴");
             //跳转activity 在fragment里面
             startActivity(new Intent(getActivity(), EmptyActivity.class));
 
         });
+        binding.button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "https://www.baidu.com";
+                OkHttpClient client = new OkHttpClient();
+
+
+                Request request = new Request.Builder()
+                        .url(url)
+                        .build();
+
+                try (Response response = client.newCall(request).execute()) {
+                    String data = response.body().string();
+                    Log.d("info", data);
+                    binding.textviewFirst.setText(data);
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
         binding.buttonFirst.setOnClickListener(view1 -> NavHostFragment.findNavController(FirstFragment.this)
                 .navigate(R.id.action_FirstFragment_to_SecondFragment));
+        binding.editText.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                Log.d("tags", String.valueOf(binding.editText.getText()));
+                binding.actBtn.setText("已经出发了额");
+                return false;
+            }
+        });
     }
 
     @Override
